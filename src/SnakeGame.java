e82e3c28e81f62edce01a1d8735407f72a8fe07c
Ali <aiimiisheikh@gmail.com>
2023-06-29 13:02:36 -0400

    Initial Commit.

---
+++ src/SnakeGame.java
@@ -0,0 +1,7 @@
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        //We used a shortcut instead of GameFrame panel = new GameFrame();
+        new GameFrame();
+    }
+}
